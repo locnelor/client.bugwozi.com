7c327b9fa8518b9121d7ae1d90bbd09e905855c7
locnelor <1750354949@qq.com>
2024-02-11 16:22:53 +0800

    update

--- components/ui/UiButton.tsx
+++ components/ui/UiButton.tsx
@@ -1,4 +1,5 @@
-import { ButtonHTMLAttributes, DetailedHTMLProps, forwardRef } from "react";
+import { ButtonHTMLAttributes, DetailedHTMLProps, forwardRef, useMemo } from "react";
+import classnames from "classnames"
 type Override<P, S> = Omit<P, keyof S> & S;
 
 const UiButton = forwardRef<
@@ -11,12 +12,31 @@ const UiButton = forwardRef<
             reset?: boolean,
             size?: "lg" | "sm" | "xs"
         }>
->(({ loading, children, submit, reset, size, disabled, className, type, ...props }, ref) => {
+>(({
+    loading,
+    children,
+    submit,
+    reset,
+    size,
+    disabled,
+    className,
+    type,
+    ...props
+}, ref) => {
+    const n = useMemo(() => {
+        const arr = ["btn"];
+        if (!!type) arr.push(`btn-${type}`)
+        if (!!size) arr.push(`btn-size`);
+        return arr.join(" ");
+    }, [className])
     return (
         <button
             {...props}
             ref={ref}
-            className={`btn ${!!type ? `btn-${type}` : ""} ${!!size ? `btn-${size}` : ""} ${className}`}
+            className={classnames(
+                n,
+                className
+            )}
             type={!!submit ? "submit" : !!reset ? "reset" : "button"}
             disabled={!!loading || disabled}
         >
