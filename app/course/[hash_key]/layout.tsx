6426bafd49f92af8ffc2c82007bb638999962950
locnelor <1750354949@qq.com>
2024-02-16 09:59:36 +0800

    chapter

--- app/course/[hash_key]/layout.tsx
+++ app/course/[hash_key]/layout.tsx
@@ -1,19 +1,64 @@
 import { LayoutProps } from "@/interfaces/page"
-import CourseMenu from "./CourseMenu"
 import CourseProvider from "./CourseProvider"
-
-const CourseIdLayout = ({
+import { gql } from "@apollo/client"
+import { getQuery } from "@/lib/client"
+import CourseEntity from "@/interfaces/CourseEntity"
+import { notFound } from "next/navigation"
+import Link from "next/link"
+const CourseHashQuery = gql`
+    query CourseHashQuery($hash_key:String!){
+        courseHashQuery(hash_key:$hash_key){
+            id
+            hash_key
+            CourseChapter{
+                name
+                id
+                createAt
+                updateAt
+                CourseContent{
+                    id
+                    name
+                    hash_key
+                }
+            }
+        }
+    }
+`
+const CourseIdLayout = async ({
     children,
     params: {
         hash_key
     }
 }: LayoutProps<{}, { hash_key: string }>) => {
+    const { data, error } = await getQuery<{
+        courseHashQuery: CourseEntity
+    }>(CourseHashQuery, { hash_key });
+    if (!data || error) return notFound()
     return (
         <div className="flex gap-4 min-h-full">
             <div className="min-h-full w-56 shadow fixed">
-                <CourseMenu
-                    hash_key={hash_key}
-                />
+                <ul className="menu rounded-box">
+                    <li>
+                        <a>简介</a>
+                    </li>
+                    {data.courseHashQuery.CourseChapter?.map(({ id, name, CourseContent }) => (
+                        <li
+                            key={`chapter_${id}`}
+                        >
+                            <a>{name}</a>
+                            <ul>
+                                {CourseContent?.map(({ name, id, hash_key }) => (
+                                    <li
+                                        key={id}
+                                        draggable
+                                    >
+                                        <Link href={`/course/${data.courseHashQuery.hash_key}/${hash_key}`}>{name}</Link>
+                                    </li>
+                                ))}
+                            </ul>
+                        </li>
+                    ))}
+                </ul>
             </div>
             <div className="grow flex justify-center">
                 <div style={{ maxWidth: "720px" }} className="w-full">
--- app/course/[hash_key]/page.tsx
+++ app/course/[hash_key]/page.tsx
@@ -1,24 +1,12 @@
-"use client"
 
-import { createEmpty } from "@/components/reactDraftEditor/DraftRichEditor"
-import dynamic from "next/dynamic"
-import { useState } from "react"
-import EditFooter from "./EditFooter"
 
-const DraftRichEditor = dynamic(() => import("@/components/reactDraftEditor/DraftRichEditor"), { ssr: false })
+
 const CourseIdPage = () => {
-    //获取文章。获取个人信息，是否可添加chapter
-    const [editorState, setEditorState] = useState(createEmpty())
+
+
     return (
         <div>
-            <DraftRichEditor
-                editorState={editorState}
-                onChange={setEditorState}
-            />
-            <EditFooter
-                editorState={editorState}
-                onChange={setEditorState}
-            />
+            123
         </div>
     )
 }
