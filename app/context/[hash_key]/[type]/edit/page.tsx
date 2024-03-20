9feeac4131e23ec04af25523b5938ab8882e0125
locnelor <1750354949@qq.com>
2024-03-20 09:08:21 +0800

    🌈 style: 添加标题

--- app/context/[hash_key]/[type]/edit/page.tsx
+++ app/context/[hash_key]/[type]/edit/page.tsx
@@ -2,12 +2,16 @@ import RichEditor from "@/components/RichEditor"
 import { PageProps } from "@/interfaces/page"
 import { getQuery } from "@/lib/client"
 import gql from "graphql-tag"
+import { Metadata } from "next"
 
 const GetSourceContextQuery = gql`
     query GetSourceContext($hash_key:String!,$type:String!){
         getSourceContext(hash_key:$hash_key,type:$type)
     }
 `
+export const metadata: Metadata = {
+    title: "文章编辑"
+}
 const ContextHashEditPage = async ({
     params: { hash_key, type }
 }: PageProps<{}, { hash_key: string, type: string }>) => {
