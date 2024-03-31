438fbbbd6c8972df3a196fb511bc48f74e9dd6fb
locnelor <1750354949@qq.com>
2024-03-31 15:29:36 +0800

    🌈 style: 维护footer

--- app/Footer.tsx
+++ app/Footer.tsx
@@ -7,18 +7,23 @@ const Footer = () => {
         <div>
             <footer className="footer relative footer-center p-10 bg-base-200 text-base-content rounded">
                 <nav className="grid grid-flow-col gap-4">
-                    <Link href="/about" className="link link-hover">关于我们</Link>
-                    <Link href="/about" className="link link-hover">课程合作</Link>
+                    <Link href="/posts/68211ab2c4825bd0634056a994ca1bd0" className="link link-hover">关于我们</Link>
+                    <Link href="/posts/85fc2525db937773ec3a890ade453007" className="link link-hover">课程合作</Link>
                     <Link target="_blank" href="https://support.qq.com/products/639696" className="link link-hover">意见反馈</Link>
                 </nav>
                 <nav>
                     <div className="grid grid-flow-col gap-4">
                         <span>友情链接:</span>
-                        <Link href="/">kgnb</Link>
+                        <span>暂无!欸嘿!</span>
+                        {/* <Link href="/">kgnb</Link> */}
                     </div>
                 </nav>
                 <aside>
-                    <p>冀ICP备2024054498号</p>
+                    <p>
+                        <Link target="_blank" href="beian.miit.gov.cn">
+                            冀ICP备2024054498号
+                        </Link>
+                    </p>
                 </aside>
             </footer>
         </div>
